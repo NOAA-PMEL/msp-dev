cb9c7037f624f7024365c446659512af09d10bed
Derek Coffman <derek.coffman@noaa.gov>
2025-05-17 00:21:18 +0000

    start datastore backend framework

--- code/apps/datastore/main.py
+++ code/apps/datastore/main.py
@@ -1,12 +1,53 @@
+import asyncio
+from datetime import datetime, timezone
+import json
+import logging
+
 from fastapi import FastAPI  # , APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 
 # from cloudevents.http import from_http
-from cloudevents.conversion import from_http
+from cloudevents.http import CloudEvent, from_http, from_json
+from cloudevents.conversion import to_structured # , from_http
+from cloudevents.exceptions import InvalidStructuredJSON
 from cloudevents.pydantic import CloudEvent
 
-# from typing import Union
-from pydantic import BaseModel
+import httpx
+from logfmter import Logfmter
+from pydantic import BaseModel, BaseSettings, Field
+
+handler = logging.StreamHandler()
+handler.setFormatter(Logfmter())
+logging.basicConfig(handlers=[handler])
+L = logging.getLogger(__name__)
+L.setLevel(logging.INFO)
+
+class Settings(BaseSettings):
+    host: str = "0.0.0.0"
+    port: int = 8000
+    debug: bool = False
+    # knative_broker: str = (
+    #     "http://kafka-broker-ingress.knative-eventing.svc.cluster.local/default/default"
+    # )
+    # mongodb_user_name: str = ""
+    # mongodb_user_password: str = ""
+    # mongodb_connection: str = (
+    #     "mongodb://uasdaq:password@uasdaq-mongodb-0.uasdaq-mongodb-svc.mongodb.svc.cluster.local:27017,uasdaq-mongodb-1.uasdaq-mongodb-svc.mongodb.svc.cluster.local:27017,uasdaq-mongodb-2.uasdaq-mongodb-svc.mongodb.svc.cluster.local:27017/data?replicaSet=uasdaq-mongodb&ssl=false"
+    # )
+    # erddap_http_connection: str = (
+    #     "http://uasdaq.pmel.noaa.gov/uasdaq/dataserver/erddap"
+    # )
+    # erddap_https_connection: str = (
+    #     "https://uasdaq.pmel.noaa.gov/uasdaq/dataserver/erddap"
+    # )
+    # # erddap_author: str = "fake_author"
+
+    dry_run: bool = False
+
+    class Config:
+        env_prefix = "DATASTORE_"
+        case_sensitive = False
+
 
 # from apis.router import api_router
 
