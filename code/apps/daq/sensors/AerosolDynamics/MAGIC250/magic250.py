f826b8e3232bd988dd02fa25715516f93e2f665b
Derek Coffman <derek.coffman@noaa.gov>
2025-07-07 12:40:05 -0700

    force rebuild

--- code/apps/daq/sensors/AerosolDynamics/MAGIC250/magic250.py
+++ code/apps/daq/sensors/AerosolDynamics/MAGIC250/magic250.py
@@ -379,7 +379,6 @@ class MAGIC250(Sensor):
         self.sensor_definition_file = "AerosolDynamics_MAGIC250_sensor_definition.json"
 
         try:
-            
             with open(self.sensor_definition_file, "r") as f:
                 self.metadata = json.load(f)
         except FileNotFoundError:
