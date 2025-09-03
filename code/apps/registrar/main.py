4d915b7e175de02ac2080a92681461806a8b8bc8
Derek Coffman <derek.coffman@noaa.gov>
2025-09-03 08:37:03 -0700

    need main too

--- code/apps/registrar/main.py
+++ code/apps/registrar/main.py
@@ -128,7 +128,8 @@ async def registry_sync(request: Request):
         L.debug(request.headers)
         L.debug("registry-sync", extra={"ce": ce, "destpath": ce["destpath"]})
         # await adapter.send_to_mqtt(ce)
-        await registrar.handle_registry_sync(ce)
+        # await registrar.handle_registry_sync(ce)
+        await registrar.sync_bcast_buffer.put(ce)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
         # msg = {"result": "OK"}
         # return get_response_event(msg, 202)
