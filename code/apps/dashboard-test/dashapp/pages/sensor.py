29da1862346338bf06663ea94632fedcfcf6b28a
hannabest <hanna.best@noaa.gov>
2025-09-15 22:17:58 +0000

    test previous state

--- code/apps/dashboard-test/dashapp/pages/sensor.py
+++ code/apps/dashboard-test/dashapp/pages/sensor.py
@@ -670,7 +670,8 @@ def build_graphs(layout_options):
                 )
             
         if ltype == "layout-3d":
-            title = f"Plots 3-D (time, {dims.keys()[0]}, {dims.keys()[1]})"
+            # title = f"Plots 3-D (time, {dims.keys()[0]}, {dims.keys()[1]})"
+            title = f"Plots 3-D (time, {dim})"
             graph_list.append(
                 dbc.AccordionItem(
                     [
@@ -681,8 +682,9 @@ def build_graphs(layout_options):
                                         "variable-list"
                                     ],
                                     xaxis="time",
-                                    yaxis={dims.keys()[0]},
-                                    zaxis={dims.keys()[1]}
+                                    # yaxis={dims.keys()[0]},
+                                    yaxis=dim
+                                    # zaxis={dims.keys()[1]}
                                 )
                             ]
                         )
