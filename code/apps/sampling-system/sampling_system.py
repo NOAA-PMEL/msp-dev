bb79095e60660bfaf13a5fb391de747df8e3a6cc
Derek Coffman <derek.coffman@noaa.gov>
2025-11-05 15:34:22 -0800

    broke something in variablemap

--- code/apps/sampling-system/sampling_system.py
+++ code/apps/sampling-system/sampling_system.py
@@ -925,7 +925,7 @@ class SamplingSystem:
             # print(f"update_variableset_by_source: {variablemap['sources']}")
             for src_xref in variablemap["sources"][source_id]:
                 self.logger.debug("update_variableset_by_source", extra={"source_xref": src_xref})
-
+                print(f"update_variableset_by_source: variablemap = {variablemap}")
                 variableset = variablemap["variablesets"][src_xref["variableset"]]
                 index_type = variableset["attributes"]["index_type"]
                 index_value = variableset["attributes"]["index_value"]
