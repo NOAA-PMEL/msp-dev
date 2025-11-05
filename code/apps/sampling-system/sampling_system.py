5f1ff7ac8155f62cfafd7722aede0dcc3f66521c
Derek Coffman <derek.coffman@noaa.gov>
2025-11-05 09:42:08 -0800

    more indexing debug

--- code/apps/sampling-system/sampling_system.py
+++ code/apps/sampling-system/sampling_system.py
@@ -1189,6 +1189,7 @@ class SamplingSystem:
         while True:
             # create timestamp for current interval and save to index values
             dt_period = round_to_nearest_N_seconds(dt=get_datetime(), Nsec=timebase)
+            self.logger.debug("index_time_monitor", extra={"timebase": timebase, "current_dt": current_dt_period, "dt_period": dt_period, "last_dt": last_dt_period})
             if dt_period != current_dt_period:
                 self.logger.debug("index_time_monitor", extra={"timebase": timebase, "current_dt": current_dt_period, "last_dt": last_dt_period})
                 last_dt_period = current_dt_period
