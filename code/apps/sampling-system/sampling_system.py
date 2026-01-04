650baf29343e70942bf5b52a99f7c26387f6b57a
Derek Coffman <derek.coffman@noaa.gov>
2026-01-04 11:05:31 -0800

    debug sampling system

--- code/apps/sampling-system/sampling_system.py
+++ code/apps/sampling-system/sampling_system.py
@@ -1535,6 +1535,16 @@ class SamplingSystem:
             # indexed_data = variablemap["indexed"]["data"][time_index["index_ready"]][variableset_name]
             indexed_data = variablemap["indexed"][index_type][index_value]["data"][target_time][variableset_name]
             
+            self.logger.debug("update_direct_variable_by_time_index", extra={"time_index": time_index})
+            self.logger.debug("update_direct_variable_by_time_index", extra={"variable_map": variablemap["indexed"][index_type]})
+            self.logger.debug("update_direct_variable_by_time_index", extra={"variable_map": variablemap["indexed"][index_type][index_value]})
+            self.logger.debug("update_direct_variable_by_time_index", extra={"variable_map": variablemap["indexed"][index_type][index_value]["data"]})
+
+            self.logger.debug("update_direct_variable_by_time_index", extra={"indexed_data": indexed_data})
+            
+            self.logger.debug("update_direct_variable_by_time_index", extra={"variableset_record": variableset_record})
+
+
             #TODO handle multi-d data
             if len(indexed_data) == 0:
                 if variableset_record["type"] in ["string", "str", "char"]:
