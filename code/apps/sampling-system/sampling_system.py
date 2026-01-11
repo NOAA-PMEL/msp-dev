a3236d4887a49cd28f8804b5cdfe80739c6173f7
Derek Coffman <derek.coffman@noaa.gov>
2026-01-10 16:11:29 -0800

    fix time data

--- code/apps/sampling-system/sampling_system.py
+++ code/apps/sampling-system/sampling_system.py
@@ -1641,7 +1641,7 @@ class SamplingSystem:
                 for vs_name, vs_data in target_variablesets[map_type].items():
                     variableset = variablemap["variablesets"][vs_name].copy()
                     if "time" not in variableset["variables"]:
-                        variableset["variables"]["time"]["data"] = target_time
+                        variableset["variables"]["time"] = {"data": target_time}
                     for v_name, v_data in vs_data.items():
                         self.logger.debug("update_variablesets_by_time_index", extra={"vs_name": vs_name, "vset": variableset})
                         self.logger.debug("update_variablesets_by_time_index", extra={"variable_updates": variable_updates})
