578777deec5751568063bd5a63b76310a09bb45c
Derek Coffman <derek.coffman@noaa.gov>
2026-06-14 08:17:44 -0700

    fix logger call

--- code/apps/daq/controllers/Synaccess/NP05B/NP05B.py
+++ code/apps/daq/controllers/Synaccess/NP05B/NP05B.py
@@ -215,16 +215,16 @@ class NP05B(Controller):
                 record["variables"]["time"]["data"] = timestamp
 
             # Status payload is like "$A0,10101\r\n"
-            self.logger("default_parse", extra={"tcp_data": tcp_data})
+            self.logger.debug("default_parse", extra={"tcp_data": tcp_data})
             if "$A0," in tcp_data:
                 try:
-                    self.logger("default_parse", extra={"tcp_data": tcp_data})
+                    self.logger.debug("default_parse", extra={"tcp_data": tcp_data})
                     init_status_data = tcp_data.split(',')[1].strip()
                     status_data = init_status_data[:5]
-                    self.logger("default_parse", extra={"init_status_data": init_status_data, "status_data": status_data})
+                    self.logger.debug("default_parse", extra={"init_status_data": init_status_data, "status_data": status_data})
 
                     status_list = [int(digit) for digit in str(status_data)]
-                    self.logger("default_parse", extra={"status_list": status_list})
+                    self.loggerv("default_parse", extra={"status_list": status_list})
                     
                     # Synaccess puts outlet 1 on the far right, so we reverse it
                     status_list.reverse() 
@@ -235,7 +235,7 @@ class NP05B(Controller):
                         
                         # 1. Update the internal state (this triggers the settings/update MQTT message)
                         if name in self.settings.get_settings():
-                            self.logger("default_parse", extra={"out_name": name, "actual": outlet_status})
+                            self.logger.debug("default_parse", extra={"out_name": name, "actual": outlet_status})
                             self.settings.set_actual(name=name, actual=outlet_status)
                             
                         # 2. Inject into the data record (only applies when metadata is included)
