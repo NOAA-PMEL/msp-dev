f4b5ca37e43a56fb1a9ab77430a8b38c876fac37
Derek Coffman <derek.coffman@noaa.gov>
2026-06-16 07:40:19 -0700

    set loglevel to INFO

--- code/apps/sampling-operations/sampling-conditions/manager.py
+++ code/apps/sampling-operations/sampling-conditions/manager.py
@@ -92,7 +92,7 @@ class SamplingCondition:
 
     def __init__(self, config, status_buffer):
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.INFO)
         self.logger.debug("SamplingCondition instantiated")
 
         self.config = config
@@ -176,7 +176,7 @@ class SamplingCondition:
             
         except Exception as e:
             self.logger.error("update method error", extra={"reason": str(e)})
-            
+
     async def evaluate_criteria(self, timestamp):
         """
         Evaluates sensor data against the condition's criteria logic.
